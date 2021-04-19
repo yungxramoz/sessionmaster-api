78f79f957cc22511c29c3e78e2a6bf47f84d32c8
Sandro Gerber <sandro-gerber@outlook.com>
2021-04-19 23:00:28 +0200

    Add database context

---
+++ SessionMaster/SessionMaster.DAL/SessionMasterContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SessionMaster.DAL
+{
+    public class SessionMasterContext : DbContext
+    {
+        protected readonly IConfiguration _configuration;
+
+        public SessionMasterContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // apply configurations
+            //modelBuilder.ApplyConfiguration(new UserConfiguration());
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString = _configuration.GetConnectionString("SessionMasterDB");
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+}
