2009f08dd5e24fec10762abcf4fa0919d2fcfc73
Sandro Gerber <sandro-gerber@outlook.com>
2021-04-19 23:00:10 +0200

    Add UnitOfWork Interface

---
+++ SessionMaster/SessionMaster.BLL/Core/UnitOfWork.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SessionMaster.BLL.Core
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        public int Complete()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
