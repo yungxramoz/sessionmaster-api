47e88d1ced9ba150bcb1bda901b37cc3a140eb85
Sandro Gerber <sandro-gerber@outlook.com>
2021-04-19 22:13:59 +0200

    Initial Project

---
+++ SessionMaster/SessionMaster.BLL/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SessionMaster.BLL
+{
+    public class Class1
+    {
+    }
+}
---
+++ SessionMaster/SessionMaster.DAL/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SessionMaster.DAL
+{
+    public class Class1
+    {
+    }
+}
