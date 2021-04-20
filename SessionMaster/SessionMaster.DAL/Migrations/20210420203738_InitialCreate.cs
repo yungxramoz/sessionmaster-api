22906de7b4c96d237a70da255652351f7e794e9d
Sandro Gerber <sandro-gerber@outlook.com>
2021-04-20 23:01:28 +0200

    Add initial migration script for the DB

---
+++ SessionMaster/SessionMaster.DAL/Migrations/20210420203738_InitialCreate.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SessionMaster.DAL.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Username = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: true),
+                    Firstname = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: true),
+                    Lastname = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: true),
+                    PasswordHash = table.Column<byte[]>(type: "varbinary(64)", maxLength: 64, nullable: true),
+                    PasswordSalt = table.Column<byte[]>(type: "varbinary(128)", maxLength: 128, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "User");
+        }
+    }
+}
