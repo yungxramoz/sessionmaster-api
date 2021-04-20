c851b6b802b4ea39b184caebfdc89c48755e38c9
Sandro Gerber <sandro-gerber@outlook.com>
2021-04-21 00:44:29 +0200

    Read controller code doc for generating swagger docs

--- SessionMaster/SessionMaster.API/Startup.cs
+++ SessionMaster/SessionMaster.API/Startup.cs
@@ -46,10 +46,10 @@ namespace SessionMaster.API
                 });
 
 
-                // Set the comments path for the Swagger JSON and UI.
-                //var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-                //var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
-                //c.IncludeXmlComments(xmlPath);
+                //Set the comments path for the Swagger JSON and UI.
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
             });
         }
 
