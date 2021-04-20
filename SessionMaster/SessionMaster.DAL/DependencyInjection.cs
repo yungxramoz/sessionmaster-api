3bf917442bb7077d1384f63666c507059b158bb2
Sandro Gerber <sandro-gerber@outlook.com>
2021-04-20 22:59:13 +0200

    Add DI for DAL and BLL

---
+++ SessionMaster/SessionMaster.DAL/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SessionMaster.DAL
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<SessionMasterContext>();
+
+            return services;
+        }
+    }
+}
