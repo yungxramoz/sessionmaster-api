d5ad05e466382a8c3b1a73029ae91445c47502dc
Sandro Gerber <sandro-gerber@outlook.com>
2021-04-23 01:05:59 +0200

    Add required attributes to user entity

--- SessionMaster/SessionMaster.DAL/Entities/User.cs
+++ SessionMaster/SessionMaster.DAL/Entities/User.cs
@@ -4,18 +4,23 @@ namespace SessionMaster.DAL.Entities
 {
     public class User : BaseEntity
     {
+        [Required]
         [StringLength(255)]
         public string Username { get; set; }
 
+        [Required]
         [StringLength(255)]
         public string Firstname { get; set; }
 
+        [Required]
         [StringLength(255)]
         public string Lastname { get; set; }
 
+        [Required]
         [StringLength(64)]
         public byte[] PasswordHash { get; set; }
 
+        [Required]
         [StringLength(128)]
         public byte[] PasswordSalt { get; set; }
     }
