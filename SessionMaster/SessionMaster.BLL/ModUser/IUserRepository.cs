21d9e2d1291408d976194e21abb0ddadbd685110
Sandro Gerber <sandro-gerber@outlook.com>
2021-04-24 20:52:45 +0200

    Update Find mehtod to return List and update Authenticate methode with proper exception handling

--- SessionMaster/SessionMaster.BLL/ModUser/IUserRepository.cs
+++ SessionMaster/SessionMaster.BLL/ModUser/IUserRepository.cs
@@ -5,5 +5,8 @@ namespace SessionMaster.BLL.ModUser
 {
     public interface IUserRepository : IGenericRepository<User>
     {
+        User Authenticate(string username, string password);
+        public User Add(User user, string password);
+        User Update(User user, string password);
     }
 }
