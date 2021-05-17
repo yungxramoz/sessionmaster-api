b392161549f47a51872e50d332ae51a8adff170b
Sandro Gerber <sandro-gerber@outlook.com>
2021-05-17 21:34:02 +0200

    Add base implementation SessionRepository

---
+++ SessionMaster/SessionMaster.BLL/ModSession/ISessionRepository.cs
@@ -0,0 +1,9 @@
+﻿using SessionMaster.BLL.Core;
+using SessionMaster.DAL.Entities;
+
+namespace SessionMaster.BLL.ModSession
+{
+    public interface ISessionRepository : IGenericRepository<Session>
+    {
+    }
+}
---
+++ SessionMaster/SessionMaster.BLL/ModSession/SessionRepository.cs
@@ -0,0 +1,13 @@
+﻿using SessionMaster.BLL.Core;
+using SessionMaster.DAL;
+using SessionMaster.DAL.Entities;
+
+namespace SessionMaster.BLL.ModSession
+{
+    public class SessionRepository : GenericRepository<Session>, ISessionRepository
+    {
+        public SessionRepository(SessionMasterContext context) : base(context)
+        {
+        }
+    }
+}
